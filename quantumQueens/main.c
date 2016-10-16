0aa0bff592b10f11e3632d11c36bb769c39a7a9c
l.banda <local_user@macbook-pro-de-administrateur-5.local>
2016-10-16 18:54:14 +0200

    add start example code and start coding

--- quantumQueens/main.c
+++ quantumQueens/main.c
@@ -6,10 +6,31 @@
 //
 //
 
+#include "quantum.h"
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+#ifdef M_PI
+#define pi M_PI
+#else
+#define pi 3.141592654
+#endif
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    quantum_reg reg;
+    int i, width=18;
+    
+    int Queen0[2] = {0,1};
+    int Queen1[2] = {2,3};
+    int Queen2[2] = {4,5};
+    int Queen3[2] = {6,7};
+    
+    
+    srand(time(0));
+    
+    reg = quantum_new_qureg(0, width);
+    
     return 0;
 }

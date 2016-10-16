39a7baec83c276000f83ac94ce1b609a1b50144d
l.banda <local_user@macbook-pro-de-administrateur-5.local>
2016-10-16 18:02:13 +0200

    Initial Commit

---
+++ quantumQueens/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  quantumQueens
+//
+//  Created by Luc on 16/10/2016.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
